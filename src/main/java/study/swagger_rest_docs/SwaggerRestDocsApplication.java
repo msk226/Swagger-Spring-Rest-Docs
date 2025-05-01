b511795c45df07f721c2ff21235e917ce7f4499d
msk226 <kminseok5167@gmail.com>
2025-05-01 18:25:06 +0900

    first commit

---
+++ src/main/java/study/swagger_rest_docs/SwaggerRestDocsApplication.java
@@ -0,0 +1,13 @@
+package study.swagger_rest_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerRestDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/swagger_rest_docs/SwaggerRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.swagger_rest_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerRestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
